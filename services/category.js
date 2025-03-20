6ca024dcb573fea6fb1e10bb7473a202100b94bc
ĐÀO VĨNH KHANG <khangdao0311@gmail.com>
2025-03-20 19:10:09 +0700

    cập nhật insert category

--- services/category.js
+++ services/category.js
@@ -9,18 +9,18 @@ module.exports = {
 
 async function insert(body) {
     try {
-        const { name, image = "", icon = '', description = "", properties = "[]" } = body
+        const { name, image = "", icon = '', description = "", proptypes = "[]" } = body
 
         if (!name) return { status: 400, message: "Tên danh mục không được trống !" }
 
-        if (!(typeof properties === 'string' && (() => { try { return Array.isArray(JSON.parse(properties)); } catch { return false; } })())) return { status: 400, message: "Property không đúng dữ liệu !" }
+        if (!(typeof proptypes === 'string' && (() => { try { return Array.isArray(JSON.parse(proptypes)); } catch { return false; } })())) return { status: 400, message: "Property không đúng dữ liệu !" }
 
         const categoryNew = new categoryModel({
             name: name,
             image: image,
             icon: icon,
             status: 1,
-            properties: JSON.parse(properties),
+            proptypes: JSON.parse(proptypes),
             description: description,
         })
 
@@ -38,11 +38,11 @@ async function update(id, body) {
         const category = await categoryModel.findById(id)
         if (!category) return { status: 400, message: "Danh mục không tồn tại !" }
 
-        const { name, image, status, icon, description, properties } = body
+        const { name, image, status, icon, description, proptypes } = body
 
         if (![0, 1, 2].includes(+status)) return { status: 400, message: "Trạng thái danh mục không hợp lệ !" }
 
-        if (!(typeof properties === 'string' && (() => { try { return Array.isArray(JSON.parse(properties)); } catch { return false; } })())) return { status: 400, message: "Property không đúng dữ liệu !" }
+        if (!(typeof proptypes === 'string' && (() => { try { return Array.isArray(JSON.parse(proptypes)); } catch { return false; } })())) return { status: 400, message: "Property không đúng dữ liệu !" }
 
         if (category.image !== "" && category.image !== image) {
             fs.unlink(`./public/images/${category.image}`, function (err) {
@@ -57,7 +57,7 @@ async function update(id, body) {
                 image: image,
                 icon: icon,
                 status: +status,
-                properties: JSON.parse(properties),
+                proptypes: JSON.parse(proptypes),
                 description: description
             }
         }, { new: true })
