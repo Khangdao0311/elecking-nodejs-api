3f135b6721d10055a3d43a2fcf1d6af0529fde09
ĐÀO VĨNH KHANG <khangdao0311@gmail.com>
2025-04-09 10:47:32 +0700

    cài thư viện mongodb và chỉnh sửa app.js

--- app.js
+++ app.js
@@ -13,6 +13,33 @@ mongoose
   .then(() => console.log("Connect successful"))
   .catch((err) => console.log("Connect fail", err));
 
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// const uri = "mongodb+srv://elecking:Elecking%40123@cluster.oj0z2vb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster";
+
+
+// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   }
+// });
+
+// async function run() {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
+//     // Send a ping to confirm a successful connection
+//     await client.db("admin").command({ ping: 1 });
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// }
+// run().catch(console.dir);
+
 var cors = require("cors");
 
 var corsOptionsDelegate = function (req, callback) {
