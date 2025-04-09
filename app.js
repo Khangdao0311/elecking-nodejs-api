475d5eb5e4dde1ebd636abb74d22f3bfb94bebd9
ĐÀO VĨNH KHANG <khangdao0311@gmail.com>
2025-04-09 22:03:03 +0700

    chỉnh sửa .env app.js

--- app.js
+++ app.js
@@ -9,36 +9,9 @@ var logger = require('morgan');
 var mongoose = require("mongoose");
 
 mongoose
-  .connect("mongodb://localhost:27017/elecking")
+  .connect(process.env.URL_MONGODB)
   .then(() => console.log("Connect successful"))
   .catch((err) => console.log("Connect fail", err));
-
-// const { MongoClient, ServerApiVersion } = require('mongodb');
-// const uri = "mongodb+srv://elecking:Elecking%40123@cluster.oj0z2vb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster";
-
-
-// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-// const client = new MongoClient(uri, {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   }
-// });
-
-// async function run() {
-//   try {
-//     // Connect the client to the server	(optional starting in v4.7)
-//     await client.connect();
-//     // Send a ping to confirm a successful connection
-//     await client.db("admin").command({ ping: 1 });
-//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     await client.close();
-//   }
-// }
-// run().catch(console.dir);
 
 var cors = require("cors");
 
