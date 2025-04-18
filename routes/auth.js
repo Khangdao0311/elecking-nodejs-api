07a77ce477b3b6cf1632e8eba270132450c8a123
ĐÀO VĨNH KHANG <khangdao0311@gmail.com>
2025-04-18 13:54:19 +0700

    cập nhật auth routers

--- routes/auth.js
+++ routes/auth.js
@@ -6,9 +6,18 @@ const authController = require('../controllers/auth')
 const { authentication, authorization } = require('../middleware/auth')
 const { upload } = require('../services/upload')
 
-router.post("/get_orders", async function (req, res, next) {
+router.get("/get_orders", async function (req, res, next) {
     try {
         const result = await authController.getOrdersQuery(req.query);
+        return res.status(result.status).json(result);
+    } catch (error) {
+        return res.status(500).json({ status: 500, message: "Internal Server Error" });
+    }
+});
+
+router.get("/get_order", async function (req, res, next) {
+    try {
+        const result = await authController.getOrder(req.query);
         return res.status(result.status).json(result);
     } catch (error) {
         return res.status(500).json({ status: 500, message: "Internal Server Error" });
